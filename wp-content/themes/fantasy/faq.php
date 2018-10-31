0ff3144171771042ed6e90c1dd8937689b3c8a48
voduycan <voduycan94@gmail.com>
2018-11-01 01:06:27 +0700

    xong wireframe

---
+++ wp-content/themes/fantasy/faq.php
@@ -0,0 +1,214 @@
+<?php
+/**
+ * Template Name: faq
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+ 
+?>
+<?php  get_header(); ?>
+<!-- Top Panel - Blockchain winter camp -->
+<section id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active" style="background-image: url(<?php bloginfo('template_url'); ?>/data/image/slide1.jpg)">
+      <img class="d-block w-100" src="" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+	    <h5>What is Lorem Ipsum?</h5>
+	    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+	    <button class="btn">take your first step</button>
+	  </div>
+    </div>
+    <div class="carousel-item" style="background-image: url(<?php bloginfo('template_url'); ?>/data/image/slide1.jpg)">
+      <img class="d-block w-100" src="" alt="Second slide">
+      <div class="carousel-caption d-none d-md-block">
+	    <h5>What is Lorem Ipsum?</h5>
+	    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+	    <button class="btn">take your first step</button>
+	  </div>
+    </div>
+    <div class="carousel-item" style="background-image: url(<?php bloginfo('template_url'); ?>/data/image/slide1.jpg)">
+      <img class="d-block w-100" src="" alt="Third slide">
+      <div class="carousel-caption d-none d-md-block">
+	    <h5>What is Lorem Ipsum?</h5>
+	    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+	    <button class="btn">take your first step</button>
+	  </div>
+    </div>
+    <div class="carousel-item" style="background-image: url(<?php bloginfo('template_url'); ?>/data/image/slide1.jpg)">
+      <img class="d-block w-100" src="" alt="Four slide">
+      <div class="carousel-caption d-none d-md-block">
+	    <h5>What is Lorem Ipsum?</h5>
+	    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+	    <button class="btn">take your first step</button>
+	  </div>
+    </div>
+    <div class="carousel-item" style="background-image: url(<?php bloginfo('template_url'); ?>/data/image/slide1.jpg)">
+      <img class="d-block w-100" src="" alt="Five slide">
+      <div class="carousel-caption d-none d-md-block">
+	    <h5>What is Lorem Ipsum?</h5>
+	    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+	    <button class="btn">take your first step</button>
+	  </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</section>
+<!-- End Top panel -->
+<!-- FAQ -->
+<section class="faq">
+	<h2>faq</h2>
+
+	<!-- Genaral -->
+	<div class="genaral">
+		<h3>genarals (6)</h3>
+		<div class="group">
+			<button class="my-btn" type="button" data-toggle="collapse" data-target="#collapseExample-g1" aria-expanded="false" aria-controls="collapseExample">
+				Button with data-target
+			</button>
+			<div class="collapse" id="collapseExample-g1">
+			  <div class="card card-body">
+			    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+			  </div>
+			</div>
+		</div>
+		<div class="group">
+			<button class="my-btn" type="button" data-toggle="collapse" data-target="#collapseExample-g2" aria-expanded="false" aria-controls="collapseExample">
+				Button with data-target
+			</button>
+			<div class="collapse" id="collapseExample-g2">
+			  <div class="card card-body">
+			    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+			  </div>
+			</div>
+		</div>
+		<div class="group">
+			<button class="my-btn" type="button" data-toggle="collapse" data-target="#collapseExample-g3" aria-expanded="false" aria-controls="collapseExample">
+				Button with data-target
+			</button>
+			<div class="collapse" id="collapseExample-g3">
+			  <div class="card card-body">
+			    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+			  </div>
+			</div>
+		</div>
+		<div class="group">
+			<button class="my-btn" type="button" data-toggle="collapse" data-target="#collapseExample-g4" aria-expanded="false" aria-controls="collapseExample">
+				Button with data-target
+			</button>
+			<div class="collapse" id="collapseExample-g4">
+			  <div class="card card-body">
+			    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+			  </div>
+			</div>
+		</div>
+		<div class="group">
+			<button class="my-btn" type="button" data-toggle="collapse" data-target="#collapseExample-g5" aria-expanded="false" aria-controls="collapseExample">
+				Button with data-target
+			</button>
+			<div class="collapse" id="collapseExample-g5">
+			  <div class="card card-body">
+			    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+			  </div>
+			</div>
+		</div>
+		<div class="group">
+			<button class="my-btn" type="button" data-toggle="collapse" data-target="#collapseExample-g6" aria-expanded="false" aria-controls="collapseExample">
+				Button with data-target
+			</button>
+			<div class="collapse" id="collapseExample-g6">
+			  <div class="card card-body">
+			    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+			  </div>
+			</div>
+		</div>
+	</div>
+	<!-- End Genaral -->
+	<!-- Application -->
+	<div class="genaral">
+		<h3>the application process (9)</h3>
+		<div class="group">
+			<button class="my-btn" type="button" data-toggle="collapse" data-target="#collapseExample-a1" aria-expanded="false" aria-controls="collapseExample">
+				Button with data-target
+			</button>
+			<div class="collapse" id="collapseExample-a1">
+			  <div class="card card-body">
+			    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+			  </div>
+			</div>
+		</div>
+		<div class="group">
+			<button class="my-btn" type="button" data-toggle="collapse" data-target="#collapseExample-a2" aria-expanded="false" aria-controls="collapseExample">
+				Button with data-target
+			</button>
+			<div class="collapse" id="collapseExample-a2">
+			  <div class="card card-body">
+			    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+			  </div>
+			</div>
+		</div>
+	</div>
+	<!-- End Application -->
+	<!-- travel -->
+	<div class="genaral">
+		<h3>the application process (9)</h3>
+		<div class="group">
+			<button class="my-btn" type="button" data-toggle="collapse" data-target="#collapseExample-t1" aria-expanded="false" aria-controls="collapseExample">
+				Button with data-target
+			</button>
+			<div class="collapse" id="collapseExample-t1">
+			  <div class="card card-body">
+			    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+			  </div>
+			</div>
+		</div>
+		<div class="group">
+			<button class="my-btn" type="button" data-toggle="collapse" data-target="#collapseExample-t2" aria-expanded="false" aria-controls="collapseExample">
+				Button with data-target
+			</button>
+			<div class="collapse" id="collapseExample-t2">
+			  <div class="card card-body">
+			    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+			  </div>
+			</div>
+		</div>
+	</div>
+	<!-- End travel -->
+</section>
+
+<!-- Journey -->
+<section class="journey">
+  <div class="row d-flex">
+    <div class="col-12 col-sm-6 text">
+      <h2>Lorem Ipsum is simply dummy.</h2>
+      <ul>
+        <li>Lorem Ipsum is not simply random text</li>
+        <li>Lorem Ipsum is not simply random text</li>
+      </ul>
+      <div class="button">
+        <button class="btn">apply now</button>
+        <button class="btn">learn about our camp</button>
+      </div>
+    </div>
+    <div class="col-12 col-sm-6 image">
+      <img src="<?php bloginfo('template_url'); ?>/data/image/slide1.jpg">
+    </div>
+  </div>  
+</section>
+<!-- end FAQ -->
+<?php  get_footer(); ?>
--- wp-content/themes/fantasy/product.php
+++ wp-content/themes/fantasy/product.php
@@ -97,25 +97,209 @@
 </section>
 <!-- End Blockchain coding -->
 <!-- Slide -->
-<section class="autoplay slider">
+<section class="curriculum text-center">
+  <h2>A curriculum specifically focused on real-life application.</h2>
+  <p>We blend intense yet immersive learning with real-life experience. That is the only way you can have both the knowledge and skill set to excel as a professional.</p>
+  <h3>Foundation Theory</h3>
+  <div class="foundation slider">
     <div>
-      <img src="http://placehold.it/350x300?text=1">
+      <div class="card">
+        <img class="card-img-top" src="http://placehold.it/350x300?text=1" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
     </div>
     <div>
-      <img src="http://placehold.it/350x300?text=2">
+      <div class="card">
+        <img class="card-img-top" src="http://placehold.it/350x300?text=1" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
     </div>
     <div>
-      <img src="http://placehold.it/350x300?text=3">
+      <div class="card">
+        <img class="card-img-top" src="http://placehold.it/350x300?text=1" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
     </div>
     <div>
-      <img src="http://placehold.it/350x300?text=4">
+      <div class="card">
+        <img class="card-img-top" src="http://placehold.it/350x300?text=1" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
     </div>
     <div>
-      <img src="http://placehold.it/350x300?text=5">
+      <div class="card">
+        <img class="card-img-top" src="http://placehold.it/350x300?text=1" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
     </div>
     <div>
-      <img src="http://placehold.it/350x300?text=6">
+      <div class="card">
+        <img class="card-img-top" src="http://placehold.it/350x300?text=1" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
     </div>
-  </section>
+  </div>
+  <button class="btn">request detailed curriculum</button>
+</section>
 <!-- End Slide -->
+<!-- Blockchain -->
+<section class="blockchain text-centerx">
+  <h2>Lorem Ipsum is simply dummy text of the printing and typesetting</h2>
+  <div class="row">
+    <div class="col-12 col-sm-6">
+      <iframe width="420" height="315" 
+      src="https://www.youtube.com/embed/tgbNymZ7vqY">
+      </iframe>
+    </div>
+    <div class="col-12 col-sm-6 text-left">
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+    </div>
+  </div>
+</section>
+<!-- End Blockchain -->
+<!-- our staff -->
+<section class="our-staff">
+  <h2>our staffs</h2>
+  <div class="staff slider">
+    <div>
+      <div class="card">
+        <div class="top">
+          <img src="http://placehold.it/350x300?text=1" alt="">
+          <div class="info">
+            <h3>Name name</h3>
+            <span>Co-founder/ CEO of dewolfe, harvard</span>
+          </div>
+        </div>
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="card">
+        <div class="top">
+          <img src="http://placehold.it/350x300?text=1" alt="">
+          <div class="info">
+            <h3>Name name</h3>
+            <span>Co-founder/ CEO of dewolfe, harvard</span>
+          </div>
+        </div>
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="card">
+        <div class="top">
+          <img src="http://placehold.it/350x300?text=1" alt="">
+          <div class="info">
+            <h3>Name name</h3>
+            <span>Co-founder/ CEO of dewolfe, harvard</span>
+          </div>
+        </div>
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div>
+     <div class="card">
+        <div class="top">
+          <img src="http://placehold.it/350x300?text=1" alt="">
+          <div class="info">
+            <h3>Name name</h3>
+            <span>Co-founder/ CEO of dewolfe, harvard</span>
+          </div>
+        </div>
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="card">
+        <div class="top">
+          <img src="http://placehold.it/350x300?text=1" alt="">
+          <div class="info">
+            <h3>Name name</h3>
+            <span>Co-founder/ CEO of dewolfe, harvard</span>
+          </div>
+        </div>
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div>
+      <div class="card">
+        <div class="top">
+          <img src="http://placehold.it/350x300?text=1" alt="">
+          <div class="info">
+            <h3>Name name</h3>
+            <span>Co-founder/ CEO of dewolfe, harvard</span>
+          </div>
+        </div>
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="trips">
+  <h2>trips in vietnam</h2>
+  <div class="slider slider-for">
+    <div class="image-for" style="background-image: url('http://placehold.it/350x300?text=1');"></div>
+    <div class="image-for" style="background-image: url('<?php bloginfo('template_url'); ?>/data/image/slide1.jpg');"></div>
+    <div class="image-for" style="background-image: url('<?php bloginfo('template_url'); ?>/data/image/slide1.jpg');"></div>
+    <div class="image-for" style="background-image: url('<?php bloginfo('template_url'); ?>/data/image/slide1.jpg');"></div>
+    <div class="image-for" style="background-image: url('<?php bloginfo('template_url'); ?>/data/image/slide1.jpg');"></div>
+  </div>
+  <div class="slider slider-nav">
+    <div class="image-nav" style="background-image: url('http://placehold.it/350x300?text=1');"></div>
+    <div class="image-nav" style="background-image: url('<?php bloginfo('template_url'); ?>/data/image/slide1.jpg');"></div>
+    <div class="image-nav" style="background-image: url('<?php bloginfo('template_url'); ?>/data/image/slide1.jpg');"></div>
+    <div class="image-nav" style="background-image: url('<?php bloginfo('template_url'); ?>/data/image/slide1.jpg');"></div>
+    <div class="image-nav" style="background-image: url('<?php bloginfo('template_url'); ?>/data/image/slide1.jpg');"></div>
+  </div>
+</section>
+<!-- End our staff -->
+<!-- Journey -->
+<section class="journey">
+  <div class="row d-flex">
+    <div class="col-12 col-sm-6 text">
+      <h2>Lorem Ipsum is simply dummy.</h2>
+      <ul>
+        <li>Lorem Ipsum is not simply random text</li>
+        <li>Lorem Ipsum is not simply random text</li>
+      </ul>
+      <div class="button">
+        <button class="btn">apply now</button>
+        <button class="btn">learn about our camp</button>
+      </div>
+    </div>
+    <div class="col-12 col-sm-6 image">
+      <img src="<?php bloginfo('template_url'); ?>/data/image/slide1.jpg">
+    </div>
+  </div>  
+</section>
 <?php  get_footer(); ?>
